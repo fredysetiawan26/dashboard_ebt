d958fada4a88df1c7a2eed7b539a326b99577012
fredysetiawan26 <setiawannfredy@gmail.com>
2023-03-06 19:15:19 +0700

    Perubahan Versi Docker-compose & IP

--- monitoring_ebt/src/utils/URL.js
+++ monitoring_ebt/src/utils/URL.js
@@ -1,10 +1,20 @@
-export const API_URL_SURYA_AC = "http://localhost:5000/ebt?data=suryaAC";
-export const API_URL_SURYA_DC = "http://localhost:5000/ebt?data=suryaDC";
-export const API_URL_TURBIN = "http://localhost:5000/ebt?data=turbin";
-export const API_URL_DOWNLOAD = "http://localhost:5000/ebt/download";
-export const API_URL_HARIAN = "http://localhost:5000/ebt/harian?";
-export const API_URL_BULANAN = "http://localhost:5000/ebt/akumulasi/harian/";
-export const API_URL_TAHUNAN = "http://localhost:5000/ebt/akumulasi/bulanan/";
-export const API_URL_POTENSI = "http://localhost:3001/site/outdoor/";
-export const API_URL_POTENSI_IRADIASI = "http://localhost:3001/site/outdoor/solar_power";
+// export const API_URL_SURYA_AC = "http://localhost:5000/ebt?data=suryaAC";
+// export const API_URL_SURYA_DC = "http://localhost:5000/ebt?data=suryaDC";
+// export const API_URL_TURBIN = "http://localhost:5000/ebt?data=turbin";
+// export const API_URL_DOWNLOAD = "http://localhost:5000/ebt/download";
+// export const API_URL_HARIAN = "http://localhost:5000/ebt/harian?";
+// export const API_URL_BULANAN = "http://localhost:5000/ebt/akumulasi/harian/";
+// export const API_URL_TAHUNAN = "http://localhost:5000/ebt/akumulasi/bulanan/";
+// export const API_URL_POTENSI = "http://localhost:3001/site/outdoor/";
+// export const API_URL_POTENSI_IRADIASI = "http://localhost:3001/site/outdoor/solar_power";
+
+export const API_URL_SURYA_AC = "http://10.46.10.128:5000/ebt?data=suryaAC";
+export const API_URL_SURYA_DC = "http://10.46.10.128:5000/ebt?data=suryaDC";
+export const API_URL_TURBIN = "http://10.46.10.128:5000/ebt?data=turbin";
+export const API_URL_DOWNLOAD = "http://10.46.10.128:5000/ebt/download";
+export const API_URL_HARIAN = "http://10.46.10.128:5000/ebt/harian?";
+export const API_URL_BULANAN = "http://10.46.10.128:5000/ebt/akumulasi/harian/";
+export const API_URL_TAHUNAN = "http://10.46.10.128:5000/ebt/akumulasi/bulanan/";
+export const API_URL_POTENSI = "http://10.46.10.128:3001/site/outdoor/";
+export const API_URL_POTENSI_IRADIASI = "http://10.46.10.128:3001/site/outdoor/solar_power";
 
